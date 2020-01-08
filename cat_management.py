678e0adc02776254efa60a5f46066edabbc7a432
bellsmarket <bellsmarketweb@gmail.com>
2020-01-09 04:07:31 +0900

    各パラメータの保存実装済

--- cat_management.py
+++ cat_management.py
@@ -4,12 +4,66 @@ import time
 import sys
 from datetime import datetime, timedelta
 import csv
-import numpy as np
+import os
+import sys
+from time import sleep
+import Adafruit_DHT
+from datetime import datetime
+
 
 d = datetime.today()
 exetime = d.strftime("%y/%m/%d %H:%M")
 
-# exit()
+
+HOMEPATH = os.environ['HOME']
+os.chdir(HOMEPATH + '/Documents')
+CSVPATH = os.getcwd()
+CSV = CSVPATH + '/cat-water-management.csv'
+
+# ---------------------------------------------------------------------------
+sensor_args = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22,
+                '2302': Adafruit_DHT.AM2302 }
+
+nowtime =  datetime.now().strftime("%Y/%m/%d %H:%M")
+
+sensor = Adafruit_DHT.AM2302
+
+pin = 4
+ #   print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
+  #  print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
+   # sys.exit(1)
+
+# Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+data = {
+    'date': '',
+    'temp': '',
+    'humi': ''
+}
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+# if humidity is not None and temperature is not None:
+#     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+# else:
+#     print('Failed to get reading. Try again!')
+context = 'Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity)
+
+
+data['date'] = nowtime
+data['temp'] = round(float('{}'.format(temperature)), 1)
+data['humi'] = round(float('{}'.format(humidity)), 1) 
+# cols = data.keys()
+row = []
+
+# -------------------------------------------------------
+
+
+
+
+
 
 EMULATE_HX711=False
 
@@ -26,7 +80,6 @@ def cleanAndExit():
 
     if not EMULATE_HX711:
         GPIO.cleanup()
-        
     print("Bye!")
     sys.exit()
 
@@ -72,37 +125,56 @@ hx.reset()
 
 # hx.tare()
 
-print("Tare done! Add weight now...")
+print("日時・室温・水の量を測量しています。")
 
 # to use both channels, you'll need to tare them both
 #hx.tare_A()
 #hx.tare_B()
 array = []
 tmp = 0
-while True:
-    try:
-        water = hx.get_weight(1) - 2596 - bowl
+# while True:
+#     try:
+#         water = hx.get_weight(1) - 2596 - bowl
 
-        # if tmp != water:
-        #     print("水の量は" + str(abs(water + 188)) + "gです。")
-        #else:
-        #    print("Same Weight!!!")
-        #print("tmp" + str(tmp))
-        print("重さは" + str(water))
+#         # if tmp != water:
+#         #     print("水の量は" + str(abs(water + 188)) + "gです。")
+#         #else:
+#         #    print("Same Weight!!!")
+#         #print("tmp" + str(tmp))
+#         # print("重さは" + str(water))
 
-        array.append(water)
+#         # array.append(water)
 
 
-        hx.power_down()
-        hx.power_up()
-        time.sleep(0.3)
-        tmp = water
-    except (KeyboardInterrupt, SystemExit):
-        # result = numpy.array(array)
-        ave = sum(array) / len(array)
-        with open('measurement.csv', "a")as f:
-            w = csv.writer(f, lineterminator='\n')
-            w.writerow(exetime)
-            w.writerow(array)
-        print(ave)
-        cleanAndExit()
+#         hx.power_down()
+#         hx.power_up()
+#         time.sleep(0.3)
+#         tmp = water
+#     except (KeyboardInterrupt, SystemExit):
+#         # result = numpy.array(array)
+#         ave = sum(array) / len(array)
+#         with open('measurement.csv', "a")as f:
+#             w = csv.writer(f, lineterminator='\n')
+#             w.writerow(exetime)
+#             w.writerow(array)
+#             w.write(data['temp'])
+
+#         print(ave)
+#         cleanAndExit()
+
+
+water = hx.get_weight(1) - 2596 - bowl
+standard_point  = hx.get_weight(1) 
+errorrange = hx.get_weight(1) - 2596
+
+print("水の量は" + str(water) + "gです")
+data = [exetime, str(data['temp']), str(water), str(standard_point), str(errorrange)]
+for v in data:
+    print(type(v))
+with open(CSV, "a")as f:
+    w = csv.writer(f, lineterminator='\n')
+    w.writerow(data)
+# print("書き込みが完了しました。")
+
+cleanAndExit()
+
